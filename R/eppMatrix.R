3ced5cbfc697e270c8c2c95cb61d262a19af60b9
ValcuMihai <valcu.mihai@gmail.com>
2013-12-19 23:06:38 +0100

    Rd files updates

--- R/eppMatrix.R
+++ R/eppMatrix.R
@@ -6,7 +6,7 @@ eppMatrix <- function(data,  pairs = ~ male + female) {
 	m = as.character(pairs[[2]][2])
 	f  = as.character(pairs[[2]][3])
   
-  # remove repeated lines, warn
+  # TODO: remove repeated lines, warn
   
   new('eppMatrix', male = as.character(data[, m]), female = as.character(data[, f]) )
 	
